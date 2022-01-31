a385ee2387a01ea72665618874a452fc34659450
Prashant Singh <59484201+prashanthakur@users.noreply.github.com>
2022-01-31 18:01:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -25,13 +25,6 @@ app.use(require('./routes/post'))
 app.use(require('./routes/user'))
 
 
-if(process.env.NODE_ENV=="production"){
-    app.use(express.static('client/build'))
-    const path = require('path')
-    app.get("*",(req,res)=>{
-        res.send(path.resolve(__dirname,'client','build','index.html'))
-    })
-}
 
 app.listen(PORT,()=>{
     console.log("server is running on",PORT)
